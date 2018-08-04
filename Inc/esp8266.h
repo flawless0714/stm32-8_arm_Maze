39db6d667eca01118a9665551b34aef9da4b0e29
DESKTOP-4PDFIRQ\user <flawless0714@gmail.com>
2018-08-04 16:40:17 +0800

    first release with many changes, same version as Src file

--- Inc/esp8266.h
+++ Inc/esp8266.h
@@ -7,8 +7,117 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
+#include "maze.h"
+/* Macros ------------------------------------------------------------------*/
+#define SSID "CCC_Lab\0"
+#define PASSWORD "00001111\0"
+#define MODULE_ACK  "ACK\0"
+#define RECV_BUFFER_SIZE            64
+#define SEND_BUFFER_SIZE            64
+#define TYPICAL_RECEIVE_SIZE        6   /* for OK-like command's return */
+#define ECHO_TURN_OFF_EDGE          13  /* at echo turn off edge, 8266 will have last echo so that the buffer should larger than typical */
+#define DEVICE_CONNECTED_SIZE       37  /* only for one device */
+#define DEVICE_CONN_ESTABLISH_SIZE  17  /* indicate that connection is established */
+#define KNOCK_MAXIMUM_WAIT_TIME     3510
+#define KNOCK_RECEIVE_SIZE          17  /* include food arm location */
+#define SEND_PACKET_WITH_MODULE     8
+#define PACKET_SEND_INTERVAL        400
+#define SEND_DATA_COMMAND_WAIT      300
+#define MODULE_RECEIVED_DATA        14
+#define PACKET_CRITICAL_WAIT        50
+#define SPECIAL_PACKET_SIZE         43  /* since we cant solve too high data rate, so this size received recv 64 bytes and msg from PC which is ACK  */
+/* AT command ----------------------------------------------------------*/
+#define AT_RST      "AT+RST\0"
+#define AT          "AT\0"
+#define AT_OK       "OK\0"
+#define AT_CIPMUX   "AT+CIPMUX\0" /* should set to 1 since we need multiple connection, note that single connection maybe also work */
+#define AT_NO_ECHO  "ATE0\0"
+#define AT_CWMODE   "AT+CWMODE\0"
+#define AT_CWSAP    "AT+CWSAP\0"
+#define AT_CWDHCP   "AT+CWDHCP\0"
+#define AT_CIPSTART "AT+CIPSTART\0"
+#define AT_CWLIF    "AT+CWLIF\0"
+#define AT_CIPSEND  "AT+CIPSEND\0"
+#define AT_CIPCLOSE "AT+CIPCLOSE\0"
 
+typedef enum
+{
+  FAILED = -1,
+  SUCCESSED = 0 /* the name is used somewhere hence we plus "ED" */
+}StatusTypeDef;
 /* Function declaration-------------------------------------------------------*/
+void wifi_Init(void);
+void wifi_process(void);
+void wifi_sendCommand(uint8_t* command, uint8_t* specific);
+void wifi_recvCheck(void);
+StatusTypeDef wifi_waitForResponse(void);
+void wifi_waitForConnect(void);
+void wifi_establishConnetion(void);
+void wifi_knockWait(void);
+void wifi_sendRawData(uint8_t* data);
+void wifi_trainingDataEncode(void);
+void wifi_sendData(void);
+StatusTypeDef wifi_parseFoodArm(void);
+void wifi_startButtonWait(void);
+
+/* Enumerator -------------------------------------------------------*/
+/** 
+  * @brief State of AT command parser.  
+  */
+typedef enum
+{
+  WAIT_FOR_RESPONSE = 10,
+  IDLE,
+  PARSING_COMMAND
+    
+}AT_command_parser_state; 
+
+typedef enum
+{
+  RECV_ERROR = -1,   /* UART parity error, overrun..., set from UART error callback */
+  STANDBY,
+  RECV_START,
+  RECV_DONE,
+  SEND_DONE,
+  MALFORMED_RECEIVED
+}UART_state; 
+
+typedef enum
+{
+  INIT = 20,
+  ATE0_DONE,
+  CHECK_PASS,
+  CHECK_FAILED,
+  RUNNING_UNCONNECT,
+  RUNNING_CONNECTED,
+  RUNNING_ESTABLISHED,
+  RUNNING_START_BUTTON_WAITING,
+  RUNNING_TRAINING,
+  RUNNING_TRAINING_DONE,
+  DATA_SEND,
+  DATA_RECEIVED,
+  MODULE_ERROR,
+  DEBUG
+}ESP8266_state; 
+
+
+
+typedef struct
+{
+  UART_state state;
+  UART_HandleTypeDef *huart;
+  ESP8266_state wifiState;
+  uint8_t errorCount;
+  uint32_t sendTick;
+  Maze_isData_Change isDataSendRD;
+}UART;
+
+
+
+StatusTypeDef wifi_waitForResponse(void);
+
+
+
 
 #ifdef __cplusplus
 }
--- Inc/maze.h
+++ Inc/maze.h
@@ -7,39 +7,46 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
+#include "stm32f4xx_hal_uart.h"
 #include <stdbool.h>
 
-/* Function declaration-------------------------------------------------------*/
+/* Function declaration -------------------------------------------------------*/
 void Maze_Struct_Init(void);
 void Maze_Rat_Detect(void);
+void Maze_Init(void);
 /** 
   * @brief State of maze.  
   */
 typedef enum
 {
-    WAIT_FOR_RAT = 1,
-    RAT_NOT_ENTERED,
-    RAT_ENTERED,
-    TRAINING_END
+  WAIT_FOR_RAT = 1,
+  RAT_NOT_ENTERED,
+  RAT_ENTERED,
+  TRAINING_END
 }Maze_state; 
 /** 
   * @brief State of food in the arm.
   */ 
 typedef enum
 {
-    no_food,
-    food_exist,
-    food_ate
+  NO_FOOD,
+  FOOD_EXIST,
+  FOOD_ATE
 }Maze_arm_food;
 
+typedef enum
+{
+  NO = 0,
+  YES 
+}Maze_isData_Change;
 /** 
   * @brief The variability found using tick1 and tick2 with converted ADC value.
   */ 
 typedef struct
 {
-    uint32_t startTick; /* no use yet */
-    uint32_t endTick; /* no use yet  */
-    uint32_t delta;
+  uint32_t startTick; /* no use yet */
+  uint32_t endTick; /* no use yet  */
+  uint32_t delta;
 }ADC_Variability;
 
 /** 
@@ -47,21 +54,23 @@ typedef struct
   */ 
 typedef struct
 {
-    uint8_t LongTerm_err;
-    uint8_t ShortTerm_err;
-    bool    isLastEnter;           
-    Maze_arm_food Food;
-    ADC_Variability Variability;
+  uint8_t LongTerm_err;
+  uint8_t ShortTerm_err;         
+  Maze_arm_food Food;
+  ADC_Variability Variability;
 }Maze_arm;
 /** 
   * @brief Struct for 8_Arm_Maze
   */ 
 typedef struct
 {
-    Maze_arm Arm[8];
-    Maze_state State;
-    uint8_t FoodCount;
-    uint8_t LastEnteredArm;
+  Maze_arm Arm[8];
+  Maze_state State;
+  uint8_t FoodCount;
+  uint8_t LastEnteredArm;
+  uint8_t EnterSeq[50]; /* enter counts should less than 50 times */
+  uint8_t* EnterSeqPtr; /* maximux index is 49 , we should guarantee that enterSeq wont exceed 50 */
+  Maze_isData_Change isDataChange; /*  */
 }Maze;
 
 
